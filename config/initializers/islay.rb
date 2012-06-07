8680a1e983294807297176e21c3493cb9da40ef6
Luke Matthew Sutton <me@lukematthewsutton.com>
2012-06-07 12:33:16 +0930

    Apply icons to the main nav entries.

--- config/initializers/islay.rb
+++ config/initializers/islay.rb
@@ -2,6 +2,6 @@ Islay::Engine.extensions.register do |e|
   e.namespace :islay_shop
 
   # Navigation
-  e.nav_entry('Products', :catalogue)
-  e.nav_entry('Orders', :orders)
+  e.nav_entry('Products', :catalogue, :class => 'icon-archive')
+  e.nav_entry('Orders', :orders, :class => 'icon-basket')
 end
