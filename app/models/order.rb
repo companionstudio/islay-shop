635da63104e3e556ef2c0d9f0d1219b2bfb00f92
Luke Matthew Sutton <me@lukematthewsutton.com>
2012-06-20 15:21:19 +0930

    Order #dump and #load now works. The order contents can now be stuck into a JSON string and stored in the session.

--- app/models/order.rb
+++ app/models/order.rb
@@ -9,6 +9,14 @@ class Order < ActiveRecord::Base
   has_many    :bonus_items,      :class_name => 'OrderBonusItem'
   has_many    :discount_items,   :class_name => 'OrderDiscountItem'
 
+  # These are the only attributes that we want to expose publically.
+  attr_accessible(
+    :billing_country, :billing_postcode, :billing_state, :billing_street,
+    :billing_city, :email, :gift_message, :gifted_to, :is_gift, :name, :phone,
+    :shipping_city, :shipping_country, :shipping_instructions, :shipping_postcode,
+    :shipping_state, :shipping_street, :use_shipping_address, :items_dump
+  )
+
   # This association has an extra method attached to it. This is so we can
   # easily retrieve an item by it's sku_id, which is necessary for both
   # #add_item and #remove_item.
@@ -37,23 +45,17 @@ class Order < ActiveRecord::Base
   end
 
   DUMP_OPTS = {
-    :except   => [
-      :payment_token, :payment_number, :payment_name, :payment_expiry_date,
-      :order_status, :processing_status, :created_at, :updated_at, :shipping_total,
-      :product_total, :total
-    ],
-    :methods => [:use_member_address, :use_member_details],
-    :include  => {:regular_items => {:except => [:id, :order_id, :created_at, :updated_at]}}
-  }.freeze
+    root: false, :methods => :items_dump,
+    :except => [
+      :id, :product_total, :shipping_total, :total, :currency,
+      :creator_id, :updater_id, :created_at, :updated_at, :person_id
+    ]
+  }
 
   # Generates a JSON string representation of the order and it's items. It
   # only dumps the regular items and their quantities. Bonus and discount
   # details are ignored, since they are reapplied when the order is loaded.
   def dump
-    # Ignore bonus items
-    # Dump discount and regular items, but omit the type. When they're loaded
-    # again, they'll be treated as regular items. A clean slate ready for
-    # promotion application.
     to_json(DUMP_OPTS)
   end
 
@@ -118,10 +120,25 @@ class Order < ActiveRecord::Base
     self[:product_total] ||= (regular_items.map(&:total) + discount_items.map(&:total)).sum
   end
 
-  protected
+  # Provides a simplified representation of the items in an order, consolidating
+  # regular and discounted items into a single collection.
+  #
+  # It is intended to be used when dumping the order contents to JSON.
+  def items_dump
+    regular   = regular_items.map   {|item| {:sku_id => item.sku_id, :quantity => item.quantity}}
+    discount  = discount_items.map  {|item| {:sku_id => item.sku_id, :quantity => item.quantity}}
+
+    regular + discount
+  end
+
+  # When loading up an order from session, this accessor is used to generate the
+  # regular item instances on the order model.
+  def items_dump=(items)
+    items.each {|i| regular_items.build(i)}
+  end
 
   class PromotionApplyError < StandardError
-    def message
+    def to_s
       "This order has promotions applied to it. You cannot modify it without
        removing them first. Try calling #remove_promotions on the order first."
     end
@@ -141,7 +158,7 @@ class Order < ActiveRecord::Base
   def apply_promotions
     raise PromotionApplyError if @_promotions_applied
 
-    promotions = Promotions.all # Should be all current promotions
+    promotions = Promotion.all # Should be all current promotions
     promotions.each {|p| p.apply!(self) if p.qualifies?(self)}
 
     @_promotions_applied = true
--- app/models/order_basket.rb
+++ app/models/order_basket.rb
@@ -1,3 +1,2 @@
 class OrderBasket < Order
-
 end
