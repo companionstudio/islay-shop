823e6ade6b860b65a083f7b18753eff0783a2a74
Luke Matthew Sutton <me@lukematthewsutton.com>
2012-08-02 16:53:34 +0930

    Being implementing styles for product categories.

--- config/initializers/islay.rb
+++ config/initializers/islay.rb
@@ -1,6 +1,9 @@
 Islay::Engine.extensions.register do |e|
   e.namespace :islay_shop
 
+  e.admin_styles true
+  e.admin_scripts false
+
   # Navigation
   e.nav_entry('Products', :catalogue, :class => 'icon-archive')
   e.nav_entry('Orders', :orders, :class => 'icon-basket')
