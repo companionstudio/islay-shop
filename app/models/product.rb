cae8f2fe3a8f9c01aa98cac617eea93246d830a7
Luke Matthew Sutton <me@lukematthewsutton.com>
2012-08-09 12:38:16 +0930

    Begin integrating the multi-asset widget.

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,19 +6,19 @@ class Product < ActiveRecord::Base
   belongs_to :category, :class_name => 'ProductCategory', :foreign_key => 'product_category_id'
   belongs_to :range,    :class_name => 'ProductRange',    :foreign_key => 'product_range_id'
   has_many   :product_assets
-  has_many   :assets,   :through => :product_assets
+  has_many   :assets,   :through => :product_assets, :order => 'position ASC'
   has_many   :skus, :order => 'position ASC'
   has_many   :sku_assets, :through => :skus, :through => :assets
   has_many   :variants, :class_name => 'ProductVariant', :order => 'position ASC'
   has_many   :stock_logs, :through => :skus, :order => 'created_at DESC'
   has_many   :price_logs, :through => :skus, :order => 'created_at DESC'
 
-  attr_accessible :name, :description, :product_category_id, :product_range_id, :published, :status, :skus_attributes
+  attr_accessible :name, :description, :product_category_id, :product_range_id, :published, :status, :skus_attributes, :asset_ids
 
   track_user_edits
   validations_from_schema
 
-  accepts_nested_attributes_for :skus, :allow_destroy => true, :reject_if => :new_sku?
+  accepts_nested_attributes_for :skus,    :allow_destroy => true, :reject_if => :new_sku?
   validates_associated :skus
 
   before_save :store_published_at
