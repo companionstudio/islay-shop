52ddcea9548c4618ce71851b771a4ba60fac8cb4
Luke Matthew Sutton <me@lukematthewsutton.com>
2012-08-14 14:57:00 +0930

    Move the movement display into the shop helpers. Modify it to handle an optional name.

--- app/helpers/islay_shop/admin/application_helper.rb
+++ app/helpers/islay_shop/admin/application_helper.rb
@@ -1,6 +1,23 @@
 module IslayShop
   module Admin
     module ApplicationHelper
+      # Generates markup for indicating the movement of a value up or down.
+      #
+      # @param String name
+      # @param String before
+      # @param String after
+      # @param String dir
+      #
+      # @return String
+      def movement(before, after, dir, opts = {})
+        dir_span = content_tag(:span, content_tag(:span, dir), :class => "dir #{dir}")
+
+        buff = []
+        buff << "#{opts[:name]}" if opts[:name]
+        buff << "#{before} #{dir_span} #{after}"
+
+        content_tag(:span, buff.join(' ').html_safe, :class => 'indicator movement')
+      end
     end
   end
 end
