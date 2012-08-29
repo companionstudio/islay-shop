3efdce79b39717e71457207900733b0254350d7e
Luke Matthew Sutton <me@lukematthewsutton.com>
2012-08-29 12:31:17 +0930

    Scope the ordering in the product category model by the table names.

--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -8,7 +8,7 @@ class ProductCategory < ActiveRecord::Base
   friendly_id :name, :use => :slugged
   positioning :product_category
 
-  has_many    :products, :order => 'position'
+  has_many    :products, :order => 'products.position'
   belongs_to  :image,     :class_name => 'ImageAsset',       :foreign_key => 'asset_id'
   belongs_to  :parent,    :class_name => 'ProductCategory',  :foreign_key => 'product_category_id'
   has_many    :children,  :class_name => 'ProductCategory',  :foreign_key => 'product_category_id', :order => 'position'
@@ -21,6 +21,6 @@ class ProductCategory < ActiveRecord::Base
   track_user_edits
 
   def self.published
-    where(:published => true, :product_category_id => nil).order('position ASC')
+    where(:published => true, :product_category_id => nil).order('product_categories.position ASC')
   end
 end
