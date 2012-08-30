fc00f358251f11db38d65a299d95bc8ef3b1082e
Luke Matthew Sutton <me@lukematthewsutton.com>
2012-08-31 09:02:28 +0930

    Credit card blueprints actually work now.

--- spec/blueprints/credit_card_payment_blueprint.rb
+++ spec/blueprints/credit_card_payment_blueprint.rb
@@ -7,12 +7,13 @@ end
 
 CreditCardPayment.blueprint do
 
-  amount      { rand(100.00) + 1 }
-  first_name  { Faker::Name.first_name }
-  last_name   { Faker::Name.last_name }
-  number      { 'XXXX-XXXX-XXXX-1111' }
-  month       { 10 }
-  year        { 2017 }
-  gateway_id  { 'kjse0934kje0934iuhsjkkjdsf0' }
-  gateway_expiry { Time.now.next_month }
+  amount              { rand(100.00) + 1 }
+  first_name          { Faker::Name.first_name }
+  last_name           { Faker::Name.last_name }
+  number              { 'XXXX-XXXX-XXXX-1111' }
+  month               { 10 }
+  year                { 2017 }
+  verification_value  { 343 }
+  gateway_id          { 'kjse0934kje0934iuhsjkkjdsf0' }
+  gateway_expiry      { Time.now.next_month }
 end
