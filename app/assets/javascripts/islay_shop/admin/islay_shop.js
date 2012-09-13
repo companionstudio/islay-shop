b2f02749a0c45b71b2e1560965b4a3269a21540d
Luke Matthew Sutton <me@lukematthewsutton.com>
2012-09-13 10:38:16 +0930

    First cut of the JS date selector, with months implemented first.

--- app/assets/javascripts/islay_shop/admin/islay_shop.js
+++ app/assets/javascripts/islay_shop/admin/islay_shop.js
@@ -15,12 +15,235 @@ IslayShop.u = {
 };
 
 /* -------------------------------------------------------------------------- */
+/* DATE SELECTION
+/* A date picker that allows multiple modes; month-by-month, range and
+/* comparison (using two ranges)
+/* -------------------------------------------------------------------------- */
+IslayShop.DateSelection = Backbone.View.extend({
+  tagName: 'form',
+  className: 'date-selection',
+  modes: ['month', 'range', 'compare'],
+  events: {'click .toggles li': 'toggle'},
+
+  initialize: function() {
+    _.bindAll(this, 'toggle');
+    this.mode = this.options.mode || 'month';
+    this.$el.attr({action: this.options.action, method: 'get'});
+    this.toggles = {};
+    this.widgets = {
+      month: new IslayShop.DateSelection.Month({url: this.options.action}),
+      range: new IslayShop.DateSelection.Range()
+    };
+  },
+
+  toggle: function(e) {
+    var $target = $(e.target),
+        mode = $target.attr('data-mode');
+
+    if (mode !== this.mode) {
+      this.modeOn(mode);
+      this.modeOff(this.mode);
+      this.mode = mode;
+    }
+  },
+
+  modeOn: function(mode) {
+    this.toggles[mode].addClass('current');
+    this.widgets[mode].show();
+  },
+
+  modeOff: function(mode) {
+    this.toggles[mode].removeClass('current');
+    this.widgets[mode].hide();
+  },
+
+  render: function() {
+    // Render toggles
+    var list = $(this.make('ul', {'class': 'toggles'}));
+
+    _.each(this.modes, function(mode) {
+      var toggle = $(this.make('li', {'data-mode': mode}, mode));
+      this.toggles[mode] = toggle;
+      list.append(toggle);
+    }, this);
+
+    this.$el.append(list);
+
+    // Render widgets
+    _.each(this.widgets, function(widget) {
+      this.$el.append(widget.render().el);
+    }, this);
+
+    this.modeOn(this.mode);
+
+    return this;
+  }
+});
+
+IslayShop.DateSelection.Month = Backbone.View.extend({
+  tagName: 'ul',
+  className: 'month',
+  months: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+  events: {
+    'click .back': 'clickBack',
+    'click .forward': 'clickForward',
+    'click .display span': 'clickDisplay',
+    'click .backYear': 'clickYearBack',
+    'click .forwardYear': 'clickYearForward',
+    'click .months li': 'clickMonth',
+  },
+
+  initialize: function() {
+    _.bindAll(this,
+      'clickBack', 'clickForward', 'clickDisplay', 'clickYearForward',
+      'clickYearBack', 'clickMonth'
+    );
+
+    this.url = this.options.url;
+
+    this.date = new Date();
+
+    var match = this.url.match(/\/month-(\d{1,2})-(\d{4})$/);
+    if (match) {
+      this.targetYear = this.currentYear = parseInt(match[2]);
+      this.currentMonth = parseInt(match[1]) - 1;
+    }
+    else {
+      this.targetYear = this.currentYear = this.date.getFullYear();
+      this.currentMonth = this.date.getMonth();
+    }
+
+  },
+
+  show: function() {
+    this.$el.show();
+  },
+
+  hide: function() {
+    this.$menu.hide();
+    this.$el.hide();
+  },
+
+  clickBack: function() {
+    if (this.currentMonth === 0) {
+      this.goTo(11, this.currentYear - 1);
+    }
+    else {
+      this.goTo(this.currentMonth - 1, this.currentYear);
+    }
+  },
+
+  clickForward: function() {
+    if (this.currentMonth === 11) {
+      this.goTo(0, this.currentYear + 1);
+    }
+    else {
+      this.goTo(this.currentMonth + 1, this.currentYear);
+    }
+  },
+
+  clickDisplay: function() {
+    this.$menu.show();
+  },
+
+  clickYearBack: function(e) {
+    this.targetYear = this.targetYear - 1;
+    this.$year.text(this.targetYear);
+    e.preventDefault();
+  },
+
+  clickYearForward: function(e) {
+    this.targetYear = this.targetYear + 1;
+    this.$year.text(this.targetYear);
+    e.preventDefault();
+  },
+
+  clickMonth: function(e) {
+    var $target = $(e.target),
+        index = parseInt($target.attr('data-index'));
+
+    this.goTo(index, this.targetYear);
+  },
+
+  goTo: function(month, year) {
+    var url, match = this.url.match(/\/month-\d{1,2}-\d{4}$/);
+
+    if (match) {
+      url = this.url.replace(match[0], '/month-' + (month + 1) + '-' + year);
+    }
+    else {
+      url = this.url + '/month-' + (month + 1) + '-' + year;
+    }
+
+    window.location = url;
+  },
+
+  render: function() {
+    this.$display = $(this.make('span', {}, this.months[this.currentMonth] + ' ' + this.currentYear));
+    this.$menu = $(this.make('div', {'class': 'menu'})).hide();
+    this.$year = $(this.make('li', {'class': 'year'}, this.currentYear));
+
+    var years = this.make('ul', {'class': 'years'}, [
+      this.make('li', {'class': 'backYear'}),
+      this.$year[0],
+      this.make('li', {'class': 'forwardYear'})
+    ]);
+
+    this.$months = _.map(this.months, function(m, i) {
+      return $(this.make('li', {'data-index': i}, m));
+    }, this);
+
+    var months = this.make('ul', {'class': 'months'}, _.pluck(this.$months, 0));
+
+    this.$menu.append(months, years);
+
+    this.$el.append(
+      this.make('li', {'class': 'back'}),
+      this.make('li', {'class': 'display'}, [this.$display[0], this.$menu[0]]),
+      this.make('li', {'class': 'forward'})
+    );
+
+    this.hide();
+
+    return this;
+  }
+});
+
+IslayShop.DateSelection.Range = Backbone.View.extend({
+  tagName: 'ul',
+  className: 'range',
+
+  initialize: function() {
+
+  },
+
+  show: function() {
+    this.$el.show();
+  },
+
+  hide: function() {
+    this.$el.hide();
+  },
+
+  render: function() {
+    this.$el.append(
+      this.make('li', {'class': 'start'}, 'Start'),
+      this.make('li', {'class': 'end'}, 'End')
+    );
+
+    this.hide();
+
+    return this;
+  }
+});
+
+/* -------------------------------------------------------------------------- */
 /* SEGMENTED CONTROL
 /* Simplistic, report-specific segmented control.
 /* -------------------------------------------------------------------------- */
 IslayShop.SegmentedControl = Backbone.View.extend({
   tagName: 'ul',
-  className: 'segmented',
+  className: 'segmented-control',
   events: {'click li': 'click'},
 
   initialize: function() {
@@ -324,6 +547,9 @@ IslayShop.TabbedTables = Backbone.View.extend({
 $SP.where('#islay-shop-admin-reports.index').run(function() {
   var graph = new IslayShop.SeriesGraph({table: $('.series-graph')});
   var topTen = new IslayShop.TabbedTables({el: $("#top-ten")});
+
+  var dates = new IslayShop.DateSelection({action: window.location.href});
+  $('#sub-header').append(dates.render().el);
 });
 
 $SP.where('#islay-shop-admin-reports.products').run(function() {
