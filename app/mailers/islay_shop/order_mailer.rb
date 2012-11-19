6917ab3f34ac852067f0bb0f99be0cfc10a160d5
Ben Hull <ben@spookandpuff.com>
2012-11-20 01:45:25 +1030

    Use pre mailer to preflight order emails

--- app/mailers/islay_shop/order_mailer.rb
+++ app/mailers/islay_shop/order_mailer.rb
@@ -1,12 +1,19 @@
 class IslayShop::OrderMailer < ActionMailer::Base
+    
+  require 'premailer'
+
+  helper '/islay/public/application'
+
   default :from => Settings.for(:shop, :email)
 
   def thank_you(order)
     @order = order
     mail(
       :to => order.email,
-      :subject => "#{Settings.for(:islay, :name)} - Thank you for your order"
-    )
+      :subject => "#{Settings.for(:islay, :name)} - Thank you for your order",
+    ) do |format|
+      format.html {with_inline_styles render}
+    end
   end
 
   def shipped(order)
@@ -23,5 +30,9 @@ class IslayShop::OrderMailer < ActionMailer::Base
       :to => order.email,
       :subject => "#{Settings.for(:islay, :name)} - Your order has been cancelled"
     )
+  end
+
+  def with_inline_styles(html)
+    Premailer.new(html, :with_html_string => true).to_inline_css
   end
 end
