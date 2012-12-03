29ef5cf733cf3cbb3a8e860adb9236af1181d453
Luke Matthew Sutton <me@lukematthewsutton.com>
2012-12-03 11:42:46 +1030

    Update routes to use the new helpers.

--- config/routes.rb
+++ config/routes.rb
@@ -1,131 +1,132 @@
 Rails.application.routes.draw do
-  scope :module => 'islay_shop' do
-    namespace :admin do
-      range = "(/month-:year-:month)(/range/:from/:to)"
+  islay_admin 'islay_shop' do
+    range = "(/month-:year-:month)(/range/:from/:to)"
 
-      scope :path => 'reports/shop', :controller => 'reports' do
-        get range, :action => :index,    :as => 'shop_reports'
+    scope :path => 'reports/shop', :controller => 'reports' do
+      get range, :action => :index,    :as => 'shop_reports'
 
-        get "totals",                                 :action => :totals,   :as => 'order_totals'
-        get "orders#{range}",                         :action => :orders,   :as => 'order_reports'
-        get 'products',                               :action => :products, :as => 'product_reports'
-        get "products/:id#{range}",                   :action => :product,  :as => 'product_report'
-        get "products/:product_id/skus/:id#{range}",  :action => :sku,      :as => 'sku_report'
-      end
+      get "totals",                                 :action => :totals,   :as => 'order_totals'
+      get "orders#{range}",                         :action => :orders,   :as => 'order_reports'
+      get 'products',                               :action => :products, :as => 'product_reports'
+      get "products/:id#{range}",                   :action => :product,  :as => 'product_report'
+      get "products/:product_id/skus/:id#{range}",  :action => :sku,      :as => 'sku_report'
+    end
 
-      scope :path => 'catalogue' do
-        get '/' => redirect('/admin/catalogue/categories'), :as => 'catalogue'
+    scope :path => 'catalogue' do
+      get '/' => redirect('/admin/catalogue/categories'), :as => 'catalogue'
 
-        resources :products do
-          collection do
-            get '(/filter-:filter)(/sort-:sort)(/page-:page)', :action => :index, :as => 'filter_and_sort'
-            put 'position', :action => :update_position, :as => 'position'
-          end
+      resources :products do
+        collection do
+          get '(/filter-:filter)(/sort-:sort)(/page-:page)', :action => :index, :as => 'filter_and_sort'
+          put 'position', :action => :update_position, :as => 'position'
+        end
 
+        get :delete, :on => :member
+
+        resources :product_assets,  :path => 'assets'
+        resources :stock_logs,      :only => 'index'
+        resources :sku_price_logs,  :only => 'index', :path => 'price_logs'
+
+        resources :skus do
+          put 'position', :action => :update_position, :as => 'position', :on => :collection
           get :delete, :on => :member
-
-          resources :product_assets,  :path => 'assets'
-          resources :stock_logs,      :only => 'index'
-          resources :sku_price_logs,  :only => 'index', :path => 'price_logs'
-
-          resources :skus do
-            put 'position', :action => :update_position, :as => 'position', :on => :collection
-            get :delete, :on => :member
-            resources :sku_assets, :path => 'assets'
-          end
-        end
-
-        resources :product_categories, :path => 'categories' do
-          collection do
-            get '(/filter-:filter)',  :action => :index,           :as => 'filter_and_sort'
-            put 'position',           :action => :update_position, :as => 'position'
-          end
-
-          member do
-            get :delete
-            get 'products(/filter-:filter)(/sort-:sort)', :action => :show, :as => 'filter_and_sort'
-          end
-        end
-
-        resources :product_ranges, :path => 'ranges' do
-          get :delete, :on => :member
-        end
-
-        resources :stock_levels, :path => 'stock', :only => 'index' do
-          collection do
-            get '(/filter-:filter)(/sort-:sort)', :action => 'index', :as => 'filter_and_sort'
-            put '', :action => 'update'
-          end
+          resources :sku_assets, :path => 'assets'
         end
       end
 
-      scope :path => 'orders' do
-        resources :order_processes, :path => 'processing', :only => 'index' do
-          collection do
-            %w(billing packing shipping recent).each do |f|
-              get "#{f}/(sort-:sort)(/page-:page)", :action => f, :as => f, :defaults => {:filter => f}
-            end
-
-            # This is just a dummy route to help us generate the URLs for the
-            # routes above. Basically, it makes the filter and sorting helpers
-            # just work.
-            get '(/:filter)(/sort-:sort)(/page-:page)',   :action => 'index', :as => 'filter_and_sort'
-
-            put 'ship/all',     :action => 'ship_all', :as => 'ship_all'
-            put 'packing/all',  :action => 'pack_all',  :as => 'pack_all'
-          end
-
-          member do
-            get 'billing',  :action => 'review_billing', :as => 'bill'
-            put 'billing',  :action => 'bill'
-
-            put 'packing',  :action => 'pack', :as => 'pack'
-            put 'shipping', :action => 'ship', :as => 'ship'
-
-            get 'cancel',   :action => 'review_cancellation', :as => 'cancel'
-            put 'cancel',   :action => 'cancel'
-
-            put 'update',   :action => 'update', :as => 'update'
-          end
+      resources :product_categories, :path => 'categories' do
+        collection do
+          get '(/filter-:filter)',  :action => :index,           :as => 'filter_and_sort'
+          put 'position',           :action => :update_position, :as => 'position'
         end
-
-        resources :order_archives, :path => 'archives', :only => 'index' do
-          get '(/filter-:filter)(/sort-:sort)(/page-:page)',  :action => 'index', :as => 'filter_and_sort', :on => :collection
-        end
-      end
-
-      resources :orders, :only => %w(index show edit update) do
-        get '(/sort-:sort)(/page-:page)', :action => 'index', :on => :collection, :as => 'filter_and_sort'
 
         member do
-          get 'payment',  :action => 'edit_payment', :as => 'payment'
-          put 'payment',  :action => 'update_payment'
-          get 'delete'
+          get :delete
+          get 'products(/filter-:filter)(/sort-:sort)', :action => :show, :as => 'filter_and_sort'
         end
       end
 
-      resources :order_summaries, :controller => 'orders', :path => 'orders', :only => 'show'
-
-      resources :promotions
-    end # namespace
-
-    namespace :public, :path => 'order' do
-      scope :path => 'basket', :controller => 'basket' do
-        get     '/',                :action => 'contents',        :as => 'order_basket'
-        post    '/add',             :action => 'add',             :as => 'order_basket_add'
-        post    '/remove/:sku_id',  :action => 'remove',          :as => 'order_basket_remove'
-        post    '/',                :action => 'update',          :as => 'order_basket_update'
-        delete  '/',                :action => 'destroy',         :as => 'order_basket_destroy'
-        delete  '/alerts',          :action => 'destroy_alerts',  :as => 'order_basket_destroy_alerts'
+      resources :product_ranges, :path => 'ranges' do
+        get :delete, :on => :member
       end
 
-      scope :path => 'checkout', :controller => 'checkout' do
-        get   '/',                :action => 'details',         :as => 'order_checkout'
-        post  '/',                :action => 'update'
-        get   '/payment',         :action => 'payment',         :as => 'order_checkout_payment'
-        get   '/payment/process', :action => 'payment_process', :as => 'order_checkout_payment_process'
-        get   '/thank-you',       :action => 'thank_you',       :as => 'order_checkout_thank_you'
+      resources :stock_levels, :path => 'stock', :only => 'index' do
+        collection do
+          get '(/filter-:filter)(/sort-:sort)', :action => 'index', :as => 'filter_and_sort'
+          put '', :action => 'update'
+        end
       end
+    end
+
+    scope :path => 'orders' do
+      resources :order_processes, :path => 'processing', :only => 'index' do
+        collection do
+          %w(billing packing shipping recent).each do |f|
+            get "#{f}/(sort-:sort)(/page-:page)", :action => f, :as => f, :defaults => {:filter => f}
+          end
+
+          # This is just a dummy route to help us generate the URLs for the
+          # routes above. Basically, it makes the filter and sorting helpers
+          # just work.
+          get '(/:filter)(/sort-:sort)(/page-:page)',   :action => 'index', :as => 'filter_and_sort'
+
+          put 'ship/all',     :action => 'ship_all', :as => 'ship_all'
+          put 'packing/all',  :action => 'pack_all',  :as => 'pack_all'
+        end
+
+        member do
+          get 'billing',  :action => 'review_billing', :as => 'bill'
+          put 'billing',  :action => 'bill'
+
+          put 'packing',  :action => 'pack', :as => 'pack'
+          put 'shipping', :action => 'ship', :as => 'ship'
+
+          get 'cancel',   :action => 'review_cancellation', :as => 'cancel'
+          put 'cancel',   :action => 'cancel'
+
+          put 'update',   :action => 'update', :as => 'update'
+        end
+      end
+
+      resources :order_archives, :path => 'archives', :only => 'index' do
+        get '(/filter-:filter)(/sort-:sort)(/page-:page)',  :action => 'index', :as => 'filter_and_sort', :on => :collection
+      end
+    end
+
+    resources :orders, :only => %w(index show edit update) do
+      get '(/sort-:sort)(/page-:page)', :action => 'index', :on => :collection, :as => 'filter_and_sort'
+
+      member do
+        get 'payment',  :action => 'edit_payment', :as => 'payment'
+        put 'payment',  :action => 'update_payment'
+        get 'delete'
+      end
+    end
+
+    resources :order_summaries, :controller => 'orders', :path => 'orders', :only => 'show'
+
+    resources :promotions
+  end # admin
+
+  islay_public 'islay_shop' do
+    namespace :basket, :path => 'order/basket' do
+      get     '/',                :action => 'contents',        :as => 'order_basket'
+      post    '/add',             :action => 'add',             :as => 'order_basket_add'
+      post    '/remove/:sku_id',  :action => 'remove',          :as => 'order_basket_remove'
+      post    '/',                :action => 'update',          :as => 'order_basket_update'
+      delete  '/',                :action => 'destroy',         :as => 'order_basket_destroy'
+      delete  '/alerts',          :action => 'destroy_alerts',  :as => 'order_basket_destroy_alerts'
+    end
+  end
+
+  islay_secure_public 'islay_shop' do
+    namespace :checkout, :path => '/order/checkout' do
+      get   '/',                :action => 'details',         :as => 'order_checkout'
+      post  '/',                :action => 'update'
+      get   '/payment',         :action => 'payment',         :as => 'order_checkout_payment'
+      get   '/payment/process', :action => 'payment_process', :as => 'order_checkout_payment_process'
+      get   '/thank-you',       :action => 'thank_you',       :as => 'order_checkout_thank_you'
     end
   end # scope
 end # draw
+
