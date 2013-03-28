f0f9d12427c5287c0dc03821e354674b6a9f9368
Luke Matthew Sutton <me@lukematthewsutton.com>
2013-03-28 12:05:44 +1030

    Make SKUs searchable.

--- app/models/sku.rb
+++ app/models/sku.rb
@@ -2,6 +2,7 @@ class Sku < ActiveRecord::Base
   include Islay::MetaData
   include IslayShop::Statuses
   include Islay::Publishable
+  include Islay::Searchable
   include SkuDescription
 
   belongs_to :product
