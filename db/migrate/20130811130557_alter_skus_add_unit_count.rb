590c5cae9ed1f32bd2a7a92b95a5a101fbe199d0
Ben Hull <ben@spookandpuff.com>
2013-08-12 12:41:35 +0930

    Make the unit_count migration generic.

--- db/migrate/20130811130557_alter_skus_add_unit_count.rb
+++ db/migrate/20130811130557_alter_skus_add_unit_count.rb
@@ -1,12 +1,6 @@
 class AlterSkusAddUnitCount < ActiveRecord::Migration
   def up
     add_column(:skus, :unit_count, :integer, :null => false, :default => 1)
-
-    execute %{
-      UPDATE skus
-      SET unit_count = 3
-      WHERE metadata->'freight_class' = 'three_pack';
-    }
   end
 
   def down
