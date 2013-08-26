fa54feba4a03f8bc20f0aed53a33d1abe67d2d14
Ben Hull <ben@spookandpuff.com>
2013-08-26 15:59:01 +0930

    Allow check_qualification on Promotion Order Item Quantity Condition

--- app/models/promotion_order_item_quantity_condition.rb
+++ app/models/promotion_order_item_quantity_condition.rb
@@ -5,6 +5,14 @@ class PromotionOrderItemQuantityCondition < PromotionCondition
     integer :quantity, :required => true, :greater_than => 0, :default => 0
   end
 
+  def check_qualification(order)
+    if qualifies?(order)
+      result(true)
+    else
+      result(false, :insufficient_quantity)
+    end
+  end
+
   def qualifies?(order)
     order.unit_total_quantity >= quantity
   end
