6476d4816199cbced4b024f8a82f990f64ec74cc
Luke Matthew Sutton <me@lukematthewsutton.com>
2013-09-10 14:10:09 +0930

    Remove dud migration.

--- db/migrate/20130910035509_rename_price_logs_for_pricing_restructure.rb
+++
@@ -1,9 +0,0 @@
-class RenamePriceLogsForPricingRestructure < ActiveRecord::Migration
-  def up
-    rename_table(:sku_price_logs, :legacy_sku_price_logs)
-  end
-
-  def down
-    rename_table(:legacy_sku_price_logs, :sku_price_logs)
-  end
-end
