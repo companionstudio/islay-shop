44c8e91d69f046b11c67c2f8c1bb21f350c3381e
Luke Matthew Sutton <me@lukematthewsutton.com>
2013-09-20 16:01:53 +0930

    Tweak order show view to display the promotion summary.

--- app/helpers/islay_shop/admin/application_helper.rb
+++ app/helpers/islay_shop/admin/application_helper.rb
@@ -1,13 +1,20 @@
 module IslayShop
   module Admin
     module ApplicationHelper
+      # Uses the Promotions::Decorator to generate a summary for a promotion.
+      #
+      # @param Promotion promotion
+      # @return String
+      def promotion_summary(promotion)
+        Promotions::Decorator.new(promotion).summary_text
+      end
+
       # Generates markup for indicating the movement of a value up or down.
       #
       # @param String name
       # @param String before
       # @param String after
       # @param String dir
-      #
       # @return String
       def movement(before, after, dir, opts = {})
         change = after - before
