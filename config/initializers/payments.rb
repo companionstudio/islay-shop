0a6efaeef41d7b3194e4b1b2eac3f6203a72bcfa
Luke Matthew Sutton <me@lukematthewsutton.com>
2013-10-25 11:34:14 +1030

    Make the Braintree environment an explicit env variable.

--- config/initializers/payments.rb
+++ config/initializers/payments.rb
@@ -1,5 +1,5 @@
 # Grab the payment configuration details from the environment, if available.
-%w(merchant_id public_key private_key).each do |name|
+%w(env merchant_id public_key private_key).each do |name|
   var = :"braintree_#{name}"
   if Settings.defined?(:shop, var)
     IslayShop::Engine.config.payments[name.to_sym] = Settings.for(:shop, var)
--- lib/islay_shop/payments.rb
+++ lib/islay_shop/payments.rb
@@ -2,9 +2,10 @@ module IslayShop::Payments
   # Initializes a provider instance.
   #
   # @return SpookAndPay::Providers::Base
+  # @todo In the future, this should handle different providers based on config
   def payment_provider
     @payment_provider ||= SpookAndPay::Providers::Braintree.new(
-      :development,
+      IslayShop::Engine.config.payments.env.to_sym,
       :merchant_id  => IslayShop::Engine.config.payments.merchant_id,
       :public_key   => IslayShop::Engine.config.payments.public_key,
       :private_key  => IslayShop::Engine.config.payments.private_key
