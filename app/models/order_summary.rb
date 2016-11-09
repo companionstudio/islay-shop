796b81464c9fa54edca96f4859b69a884a908488
Ben Hull <ben@spookandpuff.com>
2016-11-10 10:21:57 +1030

    Provide gift status to the OrderSummary

--- app/models/order_summary.rb
+++ app/models/order_summary.rb
@@ -6,7 +6,7 @@ class OrderSummary < Order
   # @return ActiveRecord::Relation
   def self.summary
     select(%{
-      id, status, name, updated_at, reference, is_gift
+      id, status, name, updated_at, reference, is_gift,
       total,
       CASE
         WHEN orders.updater_id IS NOT NULL THEN (SELECT name FROM users WHERE id = updater_id)
@@ -45,7 +45,7 @@ class OrderSummary < Order
   def self.alt_summary
     select(%{
       id, name, updated_at, billing_street, billing_city, billing_state, reference, use_shipping_address,
-      billing_postcode, shipping_street, shipping_city, shipping_state, shipping_postcode, total, 
+      billing_postcode, shipping_street, shipping_city, shipping_state, shipping_postcode, total, is_gift,
       '$' || TRIM(TO_CHAR(total, '99,999,999.99')) AS formatted_total,
       (SELECT name FROM users WHERE id = updater_id) AS updater_name
     })
